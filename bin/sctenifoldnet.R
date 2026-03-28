4c99331622f57ddb7d41e0934115d8277f591f2e
Diego Coelho <diegomscoelho@gmail.com>
2026-03-28 18:47:32 -0300

    fix: Error on 0 counts cells

--- bin/sctenifoldnet.R
+++ bin/sctenifoldnet.R
@@ -13,12 +13,13 @@ cell_type <- sub(".RDS", "", seuratObj)
 
 sc_obj <- readRDS(seuratObj)
 
-mat <- as.matrix(sc_obj[sc_obj@misc$gene4use]@assays$RNA$data)
-
-outputH0 <- scTenifoldNet(X = mat, Y = mat, nc_nNet = 4, nc_nCells = 10, nCores = n_cores, qc = F)
-weight <- as.matrix(outputH0$tensorNetworks$X)
+mat <- as.matrix(sc_obj[sc_obj@misc$gene4use]@assays$RNA$counts)
+mat <- mat[,colSums(mat) > 30]
 
 n_cells <- dim(sc_obj)[2]
+
+outputH0 <- scTenifoldNet(X = mat, Y = mat, nc_nNet = 10, nc_nCells = n_cells/2, nCores = n_cores, qc = F)
+weight <- as.matrix(outputH0$tensorNetworks$X)
 
 # Save the object
 saveRDS(weight, file = paste0(cell_type, "_weight_sctenifoldnet_", n_cells, ".rds"))

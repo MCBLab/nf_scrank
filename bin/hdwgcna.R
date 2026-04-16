f8f1538cc4e61f8e6c241be4e98be1845590d4e5
leoshow21 <leogabri21@gmail.com>
2026-04-16 15:14:50 -0300

    feat: hdwgcna as module

--- bin/hdwgcna.R
+++ bin/hdwgcna.R
@@ -7,8 +7,9 @@ suppressPackageStartupMessages({
 
 args <- commandArgs(trailingOnly = TRUE)
 input_rds <- args[1]
-n_cores <- as.numeric(args[2])
-targets_file <- args[3]
+column <- args[2]
+n_cores <- as.numeric(args[3])
+targets_file <- args[4]
 
 allowWGCNAThreads(nThreads = n_cores)
 
@@ -20,55 +21,55 @@ if (n_cells < 150) {
   quit(save = "no", status = 0)
 }
 
-genes_4_use <- seuratObj@misc$gene4use
+gene_4_use <- seuratObj@misc$gene4use
 
 if (file.exists(targets_file)) {
   targets <- readLines(targets_file)
   targets <- targets[targets != ""]
-  genes_4_use <- unique(c(genes_4_use, intersect(targets, rownames(seuratObj))))
+  gene_4_use <- unique(c(gene_4_use, intersect(targets, rownames(seuratObj))))
 }
 
-seuratObj$wgcna_group <- "target_clone"
 nome_arquivo <- paste0(cell_type, "_weight_hdWGCNA_", n_cells, ".rds")
 
 seuratObj <- SetupForWGCNA(
   seuratObj,
   gene_select = "custom",
-  features = genes_4_use,
+  features = gene_4_use,
   wgcna_name = "network"
 )
 
 seuratObj <- MetacellsByGroups(
   seurat_obj = seuratObj,
-  group.by = "wgcna_group",
+  group.by = column,    
   k = 25,
   min_cells = 100, 
   max_shared = 10,
-  ident.group = "wgcna_group"
+  ident.group = column  
 )
 seuratObj <- NormalizeMetacells(seuratObj)
 
 seuratObj <- SetDatExpr(
   seuratObj,
-  group_name = "target_clone",
-  group.by = "wgcna_group",
+  group_name = unique(seuratObj@meta.data[[column]])[1],
+  group.by = column,
   assay = 'RNA',
   layer = 'data' 
 )
 
 seuratObj <- TestSoftPowers(seuratObj, networkType = 'signed')
 power_est <- GetActiveWGCNA(seuratObj)$sft$powerEstimate
-
 final_power <- if(is.null(power_est) || is.na(power_est) || is.infinite(power_est)) 9 else power_est
 
 seuratObj <- ConstructNetwork(
   seuratObj, 
+  soft_power = final_power,
   setDatExpr = FALSE, 
   overwrite_tom = TRUE, 
   tom_name = "network", 
-  soft_power = final_power,
-  minModuleSize = 10 
+  minModuleSize = 100 
 )
+n_metacells <- ncol(GetMetacellObject(seuratObj))
+nome_arquivo <- paste0(cell_type, "_weight_hdWGCNA_", n_metacells, ".rds")
 
 weight <- as.matrix(GetTOM(seuratObj))
 weight <- weight[colnames(weight), ]
